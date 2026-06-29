ae11001d2cca79b71e220c15d47c9aacab5e081b
sandeshmalapur <malapursandesh@gmail.com>
2026-06-29 16:43:10 +0530

    test git

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,11 +1,15 @@
 import Welcome from './components/Welcome';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
+import CompanyCard from './components/CompanyCard';
+import JobCard from './components/JobCard';
 function App() {
   return(
     <div>
       <Navbar />
       <Welcome />
+      <CompanyCard />
+      <JobCard />
       <Footer />
     </div>
   )
---
+++ frontend/talentspark/src/components/JobCard.tsx
@@ -0,0 +1,12 @@
+function JobCard() {
+    return (
+        <div>
+            <h2>Software Engineer</h2>
+            <p>Google</p>
+            <p>Banglore</p>
+            <p>5LPA</p>
+        </div>
+    )
+}
+
+export default JobCard;
