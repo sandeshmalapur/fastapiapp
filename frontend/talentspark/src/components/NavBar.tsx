306c1d714ab38901b0d8e6aecbb63224185c180b
sandeshmalapur <malapursandesh@gmail.com>
2026-06-29 16:26:01 +0530

    test git

---
+++ frontend/talentspark/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <ul>Home</ul>
+        <ul>About</ul>
+        <ul>Contact</ul>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
