be5f7550dc4fc85ddf77f92273b0e8435fcab7e1
sandeshmalapur <malapursandesh@gmail.com>
2026-06-30 12:37:21 +0530

    test git

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -3,16 +3,36 @@ import Navbar from './components/NavBar';
 import Footer from './components/Footer';
 import CompanyCard from './components/CompanyCard';
 import JobCard from './components/JobCard';
+import type {Company} from "./types/company";
+import {useEffect, useState} from "react";
+import {getCompanies} from "./Services/CompanyService";
+
 function App() {
-  return(
+  const[loading, setLoading] = useState(true);
+  const[error, setError] = useState<Error | null>(null);
+  const[companies, setCompanies] = useState<Company[]>([]);
+
+  async function fetchCompanies() {
+    setLoading(true);
+    try{
+      const data = await getCompanies();
+      setCompanies(data);
+
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
     <div>
       <Navbar />
       <Welcome />
-      <CompanyCard />
+      <CompanyCard key={companies.id} companies={companies} />
       <JobCard />
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ frontend/talentspark/src/types/job.ts
@@ -0,0 +1,9 @@
+interface Job{
+    id:number;
+    title:string;
+    description:string;
+    location:string;
+    salary:number;
+    company_id:number;
+}
+export type {Job};
