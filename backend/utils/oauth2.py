d7c1bbb8af6f2b2485bf261d1ae8786687e29157
sandeshmalapur <malapursandesh@gmail.com>
2026-07-03 14:36:51 +0530

    comp backend

--- backend/utils/oauth2.py
+++ backend/utils/oauth2.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from models.users import User
 from database import get_db
 from sqlalchemy.orm import Session
 from utils.token import verify_access_token
@@ -9,8 +10,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     current_user = verify_access_token(token, db)
-    if current_user is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
     return current_user
 
 def role_required(roles:list):
--- backend/utils/token.py
+++ backend/utils/token.py
@@ -2,12 +2,9 @@ import os
 from dotenv import load_dotenv
 from jose import jwt
 from datetime import datetime, timedelta
-from database import get_db
-from schemas.token import Token
 from models import users
 from sqlalchemy.orm import Session
-from database import get_db
-from fastapi import Depends, HTTPException
+from fastapi import HTTPException
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -20,9 +17,9 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=2
     encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_access_token(token: str, db: Session = Depends(get_db)):
+def verify_access_token(token: str, db: Session):
     to_decode = jwt.decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
-    current_user = db.query(user.User).filter(user.User.id == to_decode.get("user_id")).first()
+    current_user = db.query(users.User).filter(users.User.id == to_decode.get("user_id")).first()
     if current_user is None:
         raise HTTPException(status_code=401, detail="Invalid credentials")
     return current_user
